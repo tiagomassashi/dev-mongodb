5a2dd2cb3d8677a874562e2a99af4c1f33857b54
Tiago Massashi Nagata <59301876+tiagomassashi@users.noreply.github.com>
2022-03-20 18:30:07 -0300

    feat(*): Operação de instanciação da base de dados

---
+++ src/main/java/br/com/nagata/dev/config/Instantiation.java
@@ -0,0 +1,29 @@
+package br.com.nagata.dev.config;
+
+import br.com.nagata.dev.model.User;
+import br.com.nagata.dev.repository.UserRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+
+  private final UserRepository repository;
+
+  public Instantiation(UserRepository repository) {
+    this.repository = repository;
+  }
+
+  @Override
+  public void run(String... args) {
+    repository.deleteAll();
+
+    User maria = new User(null, "Maria Brown", "maria@gmail.com");
+    User alex = new User(null, "Alex Green", "alex@gmail.com");
+    User bob = new User(null, "Bob Grey", "bob@gmail.com");
+
+    repository.saveAll(Arrays.asList(maria, alex, bob));
+  }
+}
--- src/main/java/br/com/nagata/dev/model/User.java
+++ src/main/java/br/com/nagata/dev/model/User.java
@@ -1,5 +1,6 @@
 package br.com.nagata.dev.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -7,6 +8,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import java.io.Serializable;
 
 @Data
+@AllArgsConstructor
 @Document(collection = "user")
 public class User implements Serializable {
   private static final long serialVersionUID = 1L;
