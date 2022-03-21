f1c56ee2c5c77a7f651b7cf865b94a4e03e3e25e
Tiago Massashi Nagata <59301876+tiagomassashi@users.noreply.github.com>
2022-03-20 23:18:50 -0300

    feat(*): Consulta simples com @Query

--- src/main/java/br/com/nagata/dev/repository/PostRepository.java
+++ src/main/java/br/com/nagata/dev/repository/PostRepository.java
@@ -2,12 +2,16 @@ package br.com.nagata.dev.repository;
 
 import br.com.nagata.dev.model.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+
+  @Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+  List<Post> searchTitle(String text);
 
   List<Post> findByTitleContainingIgnoreCase(String text);
 }
